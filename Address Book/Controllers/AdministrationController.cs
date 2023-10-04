f16702d1adbfe818e0c1b10170b2f711a8bc33ec
Wisdom Tochi <agbasionyewisdom@gmail.com>
2023-10-03 23:41:31 -0700

    Address Book

--- Address Book/Controllers/AdministrationController.cs
+++ Address Book/Controllers/AdministrationController.cs
@@ -7,10 +7,13 @@ namespace Address_Book.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                            UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -50,10 +53,32 @@ namespace Address_Book.Controllers
             return View(roles);
         }
 
-        [HttpPost]
-        public IActionResult EditRole()
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
         {
-            return View();
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
         }
     }
 }
